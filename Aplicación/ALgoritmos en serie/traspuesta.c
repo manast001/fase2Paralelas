f9b574e397d3caf1838bb14df34283e85b29b484
Manuel <manast001@gmail.com>
2013-12-05 12:39:15 -0300

    Update traspuesta.c

--- Aplicación/ALgoritmos en serie/traspuesta.c
+++ Aplicación/ALgoritmos en serie/traspuesta.c
@@ -4,72 +4,74 @@
 #include <wand/MagickWand.h>
 #include <string.h>
 
-
+//traspuesta 90° 
 void imagen_traspuesta(MagickWand *imagen,MagickWand *traspuesta)
 {
-	
-	register ssize_t x;
-    size_t width;
-    ssize_t y;
-	
-	PixelIterator *iterator,*iterator2;
-	PixelWand **pixels,**pixels2;
-	
-	MagickPixelPacket pixel;
+        
+    	register ssize_t x;
+    	size_t width,width2;
+    	ssize_t y;
+        
+        PixelIterator *iterator,*iterator2;
+        PixelWand **pixels,**pixels2;
+        
+        MagickPixelPacket pixel;
 
-	MagickNewImage(traspuesta,MagickGetImageHeight(imagen),MagickGetImageWidth(imagen),NewPixelWand());
-		
-	iterator = NewPixelIterator(imagen);
-	
-	for(y=0;y<(ssize_t) MagickGetImageHeight(imagen); y++)
-	{
-		printf("%f\n",y);
-		pixels = PixelGetNextIteratorRow(iterator,&width);
-		
-		iterator2 = NewPixelIterator(traspuesta);
-		
-		if (pixels == (PixelWand **) NULL) 
-			break;
-		
-		for (x=0; x < (ssize_t) width; x++)
-		{
-			pixels2 = PixelGetNextIteratorRow(iterator2,&width);
-			
-			PixelGetMagickColor(pixels[x],&pixel);
-			PixelGetMagickColor(pixels2[y],&pixel);
-			(void) PixelSyncIterator(iterator2);
-		}
-			
-		iterator2 = DestroyPixelIterator(iterator2);
-	}
-	
-	MagickWriteImages(traspuesta,"hola.bmp",MagickTrue);
-	
-	iterator = DestroyPixelIterator(iterator);
+        MagickNewImage(traspuesta,MagickGetImageHeight(imagen),MagickGetImageWidth(imagen),NewPixelWand());
+                
+        iterator = NewPixelIterator(imagen);
+        
+        for(y=0;y<(ssize_t) MagickGetImageHeight(imagen); y++)
+        {
+
+                pixels = PixelGetNextIteratorRow(iterator,&width);
+                
+                iterator2 = NewPixelIterator(traspuesta);
+                
+                if (pixels == (PixelWand **) NULL)
+                        break;
+                
+                for (x=0; x < (ssize_t) width; x++)
+                {
+                        pixels2 = PixelGetNextIteratorRow(iterator2,&width2);
+                        
+                        PixelGetMagickColor(pixels[x],&pixel);
+                        PixelSetMagickColor(pixels2[y],&pixel);
+                        (void) PixelSyncIterator(iterator2);
+                }
+                        
+                iterator2 = DestroyPixelIterator(iterator2);
+		(void) PixelSyncIterator(iterator);
+        }
+        
+        
+        iterator = DestroyPixelIterator(iterator);
     
 }
 
 int main(int argc,char **argv)
 {
-	if(argc!=2)
-	{
-		
-	//creamos las imagenes
-		MagickWand *imagen=NewMagickWand();
-		MagickWand *traspuesta=NewMagickWand();
-	//cargamos la imagen
-		MagickReadImage(imagen,argv[1]);
-	
-	//
-		imagen_traspuesta(imagen,traspuesta);
-	
-	
-	//liberamos memoria destruyendo la imagen
-		imagen = DestroyMagickWand(imagen);
-		imagen = DestroyMagickWand(traspuesta);
-	
-	//terminamos ejecucion de la libreria
-		MagickWandTerminus();
-	}
-	return 0;
+        if(argc!=2)
+        {
+                
+        //creamos las imagenes
+                MagickWand *imagen=NewMagickWand();
+                MagickWand *traspuesta=NewMagickWand();
+        //cargamos la imagen
+                MagickReadImage(imagen,argv[1]);
+        
+        //
+                imagen_traspuesta(imagen,traspuesta);
+        
+        	MagickWriteImages(traspuesta,argv[2],MagickTrue);
+        
+        //liberamos memoria destruyendo la imagen
+                imagen = DestroyMagickWand(imagen);
+                imagen = DestroyMagickWand(traspuesta);
+                
+        
+        //terminamos ejecucion de la libreria
+                MagickWandTerminus();
+        }
+        return 0;
 }
