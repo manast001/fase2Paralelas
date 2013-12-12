eaabfcf8bac3cf39314d2f56bcdc594fd5b4814f
pabloandres <pabloandres@Snake>
2013-12-11 23:31:20 -0300

    sub. archivos

--- Interfaz de Usuario/public_html/subir_archivo.php
+++ Interfaz de Usuario/public_html/subir_archivo.php
@@ -1,7 +1,11 @@
 <?php 
  if(isset($_FILES['archivo'])){ 
-    $archivo = $_FILES['archivo']; 
-    if(move_uploaded_file($archivo['tmp_name'], "archivos_subidos/{$archivo['name']}")){
+    $archivo = $_FILES['archivo'];
+    //test de ejecucion de funciones
+    
+    exec('aplicacion/algoritmos_serie/Egrises.c',$archivo);
+    
+    if(move_uploaded_file($archivo['tmp_name'], "imagenes_generadas/{$archivo['name']}")){
        echo 1;
     }else{
        echo 0;
